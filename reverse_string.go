4b7bbece32f3d6c25b16cc5371166c0919d1cb1c
alexhaiduk <alexhaiduk82@gmail.com>
2022-05-13 13:00:44 +0300

    solution reverse

--- reverse_string.go
+++ reverse_string.go
@@ -2,8 +2,26 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	output = ""
-	for i := len(input) - 1; i >= 0; i-- {
-		output = output + string(input[i])
+	/*
+		var temp = make([]rune, len(input))
+		var i int = 0
+		for _, v := range input {
+			temp[i] = v
+			i++
+		}
+		for i = len(temp) - 1; i >= 0; i-- {
+			if temp[i] != 0 {
+				output += string(temp[i])
+			}
+		}
+		for i = 0; i < len(temp); i++ {
+			if temp[i] != 0 {
+				output = string(temp[i]) + output
+			}
+		}
+	*/
+	for _, v := range input {
+		output = string(v) + output
 	}
 	return
 }
